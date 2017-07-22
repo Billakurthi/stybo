b29523bdeaf432f7fe2d92352365b841753c0a28
sanjit <sanjitschouhan@gmail.com>
2017-07-23 02:38:57 +0530

    apiai

--- public/apiaiService.js
+++ public/apiaiService.js
@@ -1,42 +1,22 @@
-const apiai = require('apiai');
-
-const apiAiapp = apiai("30f26315bca54670ae2274a18e35bfa8");
+const apiai = new ApiAi.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
 
 exports.callApiai = function (inputString) {
 
+    return client.textRequest(inputString).then(
+        handleResponse,
+        handleError
+    );
 
-    // function responseData(response) {
-    //     console.log(response);
-    //     return response.result.fulfillment.speech;
-    // };
+    function handleResponse(serverResponse) {
+        console.log("Response from apiai: ");
+        console.log(serverResponse);
+        return "Message received";
+    }
 
-    // function errorData(error) {
-    //     console.log(error);
-    //     console.log("error from api service" + error);
-    // };
-
-    // //request.end();
-    // return apiAiapp.textRequest(inputString, {
-    //     sessionId: '<unique session id>'
-    // }).then(responseData, errorData);
-
-
-
-    return request = apiAiapp.textRequest(inputString, {
-        sessionId: '<unique session id>'
-    });
-
-    request.on('response', function (response) {
-        console.log(response);
-        return response.result.fulfillment.speech;
-    });
-
-    request.on('error', function (error) {
-        console.log(error);
-    });
-
-    request.end();
-
-
+    function handleError(serverError) {
+        console.log("Error from apiai: ");
+        console.log(serverError);
+        return "error";
+    }
 
 };
