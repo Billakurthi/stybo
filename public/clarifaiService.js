6d17828ad77c061822ec66f0689b47f6c6ab1a4a
sanjit <sanjitschouhan@gmail.com>
2017-07-23 02:08:22 +0530

    urls

--- public/clarifaiService.js
+++ public/clarifaiService.js
@@ -8,22 +8,25 @@ const appClarifai = new Clarifai.App({
 
 // Predict function to predict image
 exports.predict = function (url) {
-    function replydata(response) {
-        var reply = response.outputs[0].data.concepts[0].name +
-            " with confidence " + response.outputs[0].data.concepts[0].value;
-        console.log("Response:" + reply);
-        // sendTextMessage(senderID, reply);
-        return reply;
-    }
-
-    function errordata(err) {
-        console.log("Error:");
-        console.log(err);
-        // sendTextMessage(senderID, "I am unable to get any result from your input");
-        return "I am unable to get any result from your input";
-    }
-
-    return appClarifai.models.predict("Stybo", url).then(replydata, errordata);
+    console.log("URLS:");
+    console.log(url);
+    url = encodeURI(url);
+    console.log(url);
+    return appClarifai.models.predict("Stybo", url).then(
+        function (response) {
+            var reply = response.outputs[0].data.concepts[0].name +
+                " with confidence " + response.outputs[0].data.concepts[0].value;
+            console.log("Response:" + reply);
+            // sendTextMessage(senderID, reply);
+            return reply;
+        },
+        function (err) {
+            console.log("Error:");
+            console.log(err);
+            // sendTextMessage(senderID, "I am unable to get any result from your input");
+            // return "I am unable to get any result from your input";
+        }
+    );
 };
 
 exports.create = function (url) {
