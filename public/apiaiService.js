646757a668d4de68e7e4533657220225ac8e361b
sanjit <sanjitschouhan@gmail.com>
2017-07-23 03:14:38 +0530

    apiai

--- public/apiaiService.js
+++ public/apiaiService.js
@@ -2,24 +2,24 @@
 var apiai = require('apiai');
 
 // const apiai_app = new ApiAI.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
-var app = apiai("30f26315bca54670ae2274a18e35bfa8");
+var app_apiai = apiai("30f26315bca54670ae2274a18e35bfa8");
+
+var bodyParser = require('body-parser');
+app_apiai.use(bodyParser.json());
 
 exports.callApiai = function (inputString, callback, senderID) {
 
-    var request = app.textRequest(inputString, {
+    var request = app_apiai.textRequest(inputString, {
         sessionId: '<unique session id>'
     });
 
     request.on('response', function (response) {
-        console.log(response);
-
         console.log("Build Reply Message: " + response);
+        var reply = response.result.fulfillment.speech;
         callback(senderID, "Reply");
     });
 
     request.on('error', function (error) {
-        console.log(error);
-
         console.log("Build Error Message: " + error);
         callback(senderID, "Error");
     });
