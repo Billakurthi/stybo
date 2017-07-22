3e227494700737b2999caa46c517ad3bb21a0d5e
sanjit <sanjitschouhan@gmail.com>
2017-07-23 02:44:35 +0530

    apiai

--- public/apiaiService.js
+++ public/apiaiService.js
@@ -1,20 +1,22 @@
-const apiai = new ApiAi.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
+const ApiAi = require('api-ai-javascript');
+
+const apiai_app = new ApiAi.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
 
 exports.callApiai = function (inputString) {
 
-    return client.textRequest(inputString).then(
+    return apiai_app.textRequest(inputString).then(
         handleResponse,
         handleError
     );
 
     function handleResponse(serverResponse) {
-        console.log("Response from apiai: ");
+        console.log("Response from apiai_app: ");
         console.log(serverResponse);
         return "Message received";
     }
 
     function handleError(serverError) {
-        console.log("Error from apiai: ");
+        console.log("Error from apiai_app: ");
         console.log(serverError);
         return "error";
     }
