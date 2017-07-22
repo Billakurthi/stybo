d6aaef6793b0d681edb41beb496be79f302dc639
sanjit <sanjitschouhan@gmail.com>
2017-07-23 03:18:59 +0530

    apiai

--- public/apiaiService.js
+++ public/apiaiService.js
@@ -3,9 +3,9 @@ var apiai = require('apiai');
 
 // const apiai_app = new ApiAI.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
 var app_apiai = apiai("30f26315bca54670ae2274a18e35bfa8");
-
-var bodyParser = require('body-parser');
-app_apiai.use(bodyParser.json());
+//
+// var bodyParser = require('body-parser');
+// app_apiai.use(bodyParser.json());
 
 exports.callApiai = function (inputString, callback, senderID) {
 
