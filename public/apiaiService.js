6fae13bb2e53eb2d600a4b5f578e6e33b3b786dd
sanjit <sanjitschouhan@gmail.com>
2017-07-23 03:08:10 +0530

    apiai

--- public/apiaiService.js
+++ public/apiaiService.js
@@ -1,24 +1,46 @@
-const ApiAI = require("api-ai-javascript");
+// const ApiAI = require("api-ai-javascript");
+var apiai = require('apiai');
 
-const apiai_app = new ApiAI.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
+// const apiai_app = new ApiAI.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
+var app = apiai("30f26315bca54670ae2274a18e35bfa8");
 
-exports.callApiai = function (inputString) {
+exports.callApiai = function (inputString, callback, senderID) {
 
-    return apiai_app.textRequest(inputString).then(
-        handleResponse,
-        handleError
-    );
+    var request = app.textRequest(inputString, {
+        sessionId: '<unique session id>'
+    });
 
-    function handleResponse(serverResponse) {
-        console.log("Response from apiai_app: ");
-        console.log(serverResponse);
-        return "Message received";
-    }
+    request.on('response', function (response) {
+        console.log(response);
 
-    function handleError(serverError) {
-        console.log("Error from apiai_app: ");
-        console.log(serverError);
-        return "error";
-    }
+        console.log("Build Reply Message: " + response);
+        callback(senderID, "Reply");
+    });
+
+    request.on('error', function (error) {
+        console.log(error);
+
+        console.log("Build Error Message: " + error);
+        callback(senderID, "Error");
+    });
+
+    request.end();
+
+    // return apiai_app.textRequest(inputString).then(
+    //     handleResponse,
+    //     handleError
+    // );
+    //
+    // function handleResponse(serverResponse) {
+    //     console.log("Response from apiai_app: ");
+    //     console.log(serverResponse);
+    //     return "Message received";
+    // }
+    //
+    // function handleError(serverError) {
+    //     console.log("Error from apiai_app: ");
+    //     console.log(serverError);
+    //     return "error";
+    // }
 
 };
