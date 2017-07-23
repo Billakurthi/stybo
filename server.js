9cf6070293dd5117e26b9370f2f73de8358bafcb
Billakurthi <avinash.billakurthi@gmail.com>
2017-07-23 14:51:33 +0530

    json data images

--- server.js
+++ server.js
@@ -22,6 +22,57 @@ const apiaiService = require('./public/apiaiService');
 // };
 //user prototype object 
 var current_users = {};
+var costumes = {
+  "#Neat Hourglass": {
+    "url1": "http://ecx.images-amazon.com/images/I/71%2BQSzV%2B1cL._UL1500_.jpg",
+    "url2": "http://ecx.images-amazon.com/images/I/41nqVZKJz3L.jpg",
+    "url3": "http://ecx.images-amazon.com/images/I/61w-Hv-tKqL._UL1500_.jpg",
+    "url4": "http://ecx.images-amazon.com/images/I/61Jwjn23N9L._UL1500_.jpg",
+    "url5": "http://ecx.images-amazon.com/images/I/61kig18c9XL._UL1500_.jpg"
+  },
+  "#Inverted Triangle": {
+    "url1": "http://ecx.images-amazon.com/images/I/71ji0LPifIL._UL1500_.jpg",
+    "url2": "http://ecx.images-amazon.com/images/I/61QV3gZrwQL._UL1500_.jpg",
+    "url3": "http://ecx.images-amazon.com/images/I/51dKbbwaWnL._UL1500_.jpg",
+    "url4": "http://ecx.images-amazon.com/images/I/51itQkdgAeL._UL1500_.jpg",
+    "url5": "http://ecx.images-amazon.com/images/I/51Wp-nwOhHL._UL1500_.jpg"
+  },
+  "#Apple": {
+    "url1": "http://ecx.images-amazon.com/images/I/71YQM7ewIfL._UL1500_.jpg",
+    "url2": "http://ecx.images-amazon.com/images/I/81ivCFgPyFL._UL1500_.jpg",
+    "url3": "http://ecx.images-amazon.com/images/I/913d1hmo5dL._UL1500_.jpg",
+    "url4": "http://ecx.images-amazon.com/images/I/61GUm8nCI3L._UL1500_.jpg",
+    "url5": "http://ecx.images-amazon.com/images/I/71DsbnF1TdL._UL1500_.jpg"
+  },
+  "#Rectangle": {
+    "url1": "http://www.amazon.in/Blue-Tomatoes-Womens-Dress-BTDR5002A/dp/B01MAWLAIY/ref=sr_1_1?s=apparel&ie=UTF8&qid=1500797534&sr=1-1&nodeID=1571271031&psd=1&keywords=belted+tops+for+women",
+    "url2": "http://ecx.images-amazon.com/images/I/41WkYdsHBjL._AC_UL260_SR200,260_FMwebp_QL70_.jpg",
+    "url3": "http://ecx.images-amazon.com/images/I/41dKBRjvpjL._AC_UL260_SR200,260_FMwebp_QL70_.jpg",
+    "url4": "http://ecx.images-amazon.com/images/I/418kc0ymF3L._AC_UL260_SR200,260_FMwebp_QL70_.jpg",
+    "url5": "http://ecx.images-amazon.com/images/I/31PyhhmkJLL._AC_UL260_SR200,260_FMwebp_QL70_.jpg"
+  },
+  "#Pear": {
+    "url1": "http://ecx.images-amazon.com/images/I/61UoOsYb4RL._UL1500_.jpg",
+    "url2": "http://ecx.images-amazon.com/images/I/61Znnokj20L._UL1500_.jpg",
+    "url3": "http://ecx.images-amazon.com/images/I/71uiMIEqrVL._UL1500_.jpg",
+    "url4": "http://ecx.images-amazon.com/images/I/710HTMM4wNL._UL1500_.jpg",
+    "url5": "http://ecx.images-amazon.com/images/I/71FFrmx6msL._UL1500_.jpg"
+  },
+  "#Full Hourglass": {
+    "url1": "http://ecx.images-amazon.com/images/I/814rnf3ZXGL._UL1500_.jpg",
+    "url2": "http://ecx.images-amazon.com/images/I/71yim%2BNAOTL._UL1500_.jpg",
+    "url3": "http://ecx.images-amazon.com/images/I/91Y26g1VpyL._UL1500_.jpg",
+    "url4": "http://ecx.images-amazon.com/images/I/71-ViCtCXcL._UL1500_.jpg",
+    "url5": "http://ecx.images-amazon.com/images/I/71DbIpwn2TL._UL1500_.jpg"
+  },
+  "#Lean Column": {
+    "url1": "http://ecx.images-amazon.com/images/I/71RqTZISV5L._UL1500_.jpg",
+    "url2": "http://ecx.images-amazon.com/images/I/61hVFej6N8L._UL1500_.jpg",
+    "url3": "http://ecx.images-amazon.com/images/I/71Dl0tiqz6L._UL1500_.jpg",
+    "url4": "http://ecx.images-amazon.com/images/I/71kJ2vBpeoL._UL1500_.jpg",
+    "url5": "http://ecx.images-amazon.com/images/I/71TSo0zPzfL._UL1500_.jpg"
+  }
+}
 
 //parse text using body parser
 app.use(bodyParser.json());
@@ -139,7 +190,7 @@ function receivedMessage(event) {
           function (reply) {
             apiaiService.callApiai(reply, sendTextMessage, senderID);
             sendTextMessage(senderID, reply);
-            if (reply != '#Rejected') { sendGenericMessage(senderID); }
+            if (reply != '#Rejected') { sendGenericMessage(senderID, reply); }
           }
         );
 
@@ -200,7 +251,15 @@ function sendPlayButtons(recipientId) {
 
 // send generic message
 
-function sendGenericMessage(recipientId) {
+function sendGenericMessage(recipientId, reply) {
+
+  var sr1 = costumes[reply].url1;
+  var sr2 = costumes[reply].url2;
+  var sr3 = costumes[reply].url3;
+  var sr4 = costumes[reply].url4;
+  var sr5 = costumes[reply].url5;
+
+
   var messageData = {
     recipient: {
       id: recipientId
@@ -213,8 +272,8 @@ function sendGenericMessage(recipientId) {
           elements: [{
             title: "Exotic",
             subtitle: "Your best fit is here",
-            item_url: "http://ecx.images-amazon.com/images/I/71%2BQSzV%2B1cL._UL1500_.jpg",
-            image_url: "http://ecx.images-amazon.com/images/I/71%2BQSzV%2B1cL._UL1500_.jpg",
+            item_url: sr1,
+            image_url: sr1,
             buttons: [{
               type: "postback",
               title: "Buy Now",
@@ -223,8 +282,8 @@ function sendGenericMessage(recipientId) {
           }, {
             title: "Vibrant",
             subtitle: "Be ready for party always",
-            item_url: "http://ecx.images-amazon.com/images/I/41nqVZKJz3L.jpg",
-            image_url: "http://ecx.images-amazon.com/images/I/41nqVZKJz3L.jpg",
+             item_url: sr2,
+            image_url: sr2,
             buttons: [{
               type: "postback",
               title: "Buy Now",
@@ -233,8 +292,28 @@ function sendGenericMessage(recipientId) {
           }, {
             title: "Versatile",
             subtitle: "Get Trendy",
-            item_url: "http://ecx.images-amazon.com/images/I/61w-Hv-tKqL._UL1500_.jpg",
-            image_url: "http://ecx.images-amazon.com/images/I/61w-Hv-tKqL._UL1500_.jpg",
+             item_url: sr3,
+            image_url: sr3,
+            buttons: [{
+              type: "postback",
+              title: "Buy Now",
+              payload: "#buyClicked",
+            }]
+          },{
+            title: "Dhana Dhan",
+            subtitle: "You are princess",
+             item_url: sr4,
+            image_url: sr4,
+            buttons: [{
+              type: "postback",
+              title: "Buy Now",
+              payload: "#buyClicked",
+            }]
+          },{
+            title: "Angel here",
+            subtitle: "Rock on Style",
+             item_url: sr5,
+            image_url: sr5,
             buttons: [{
               type: "postback",
               title: "Buy Now",
