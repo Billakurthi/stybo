4c311db51fcd58a5d66065bc84321570e2624579
billakurthi <avinash.billakurthi@gmail.com>
2017-10-11 18:32:25 +0530

    auth data in seperate file

--- services/auth.js
+++ services/auth.js
@@ -9,6 +9,19 @@ module.exports = {
     facebookConfig: {
         FACEBOOK_PAGE_ACCESS_TOKEN: 'EAACvN2HxY5YBAJIBP17DJkYfyWKXNiarYFO5qaoBdHdZBDlctUCV1ZBuSsH53oH2hAIP8X0pYU2oUrLR7uygYD9LEjKZBKYiKIRBwBMO8zXl5ugRcFsOOH0grTjQSvVIzKgWX9OAMwgZBx4zcHZAGDQBuT8jG91KJLhufsL5ANwZDZD',
         FACEBOOK_VERIFY_TOKEN: 'my-secrete'
+    },
+
+    //not using google search in this project
+    googleSearchConfig: {
+        CUSTOM_SEARCH_API_KEY: 'AIzaSyApKyGupIhvaeKX-dhfsWyK3_NT1E0UuHg'
+    },
+
+    //clarify config
+
+    clarifyConfig: {
+
+        CLARIFY_API_KEY: 'bd626861f2f24192945ce59e035f0d02'
+
     }
 
 }
--- services/clarifaiService.js
+++ services/clarifaiService.js
@@ -1,62 +1,71 @@
-// Require the client
-const Clarifai = require('clarifai');
+'use strict';
+(function () {
 
 
 
-// initialize with your api key. This will also work in your browser via http://browserify.org/
-const appClarifai = new Clarifai.App({
-    apiKey: 'bd626861f2f24192945ce59e035f0d02'
-});
+    // Require the client
+    const Clarifai = require('clarifai'),
 
-// Predict function to predict image
-exports.predict = function (url) {
-    //console.log("URLS:");
-    //console.log(url);
-    url = encodeURI(url);
-    //console.log(url);
-    return appClarifai.models.predict("Stybo", url).then(
-        function (response) {
-            var reply = response.outputs[0].data.concepts[0].name;
-            // +                " with confidence " + response.outputs[0].data.concepts[0].value;
-            console.log("Response:" + reply);
-            // sendTextMessage(senderID, reply);
-            return '#' + reply;
-        },
-        function (err) {
-            console.log("Error:");
-            console.log(err);
-            // sendTextMessage(senderID, "I am unable to get any result from your input");
-            // return "I am unable to get any result from your input";
-        }
-    );
-};
+        clarifyConfig = require('./auth').clarifyConfig;
 
-exports.create = function (url) {
-    appClarifai.inputs.create({
-        url: url,
-        concepts: [
-            {
-                id: "Stybo",
-                value: true
-            }
-        ]
+
+    // initialize with your api key. This will also work in your browser via http://browserify.org/
+    const appClarifai = new Clarifai.App({
+        apiKey: clarifyConfig.CLARIFY_API_KEY
     });
-};
+
+    // Predict function to predict image
+    exports.predict = function (url) {
+        //console.log("URLS:");
+        //console.log(url);
+        url = encodeURI(url);
+        //console.log(url);
+        return appClarifai.models.predict("Stybo", url).then(
+            function (response) {
+                var reply = response.outputs[0].data.concepts[0].name;
+                // +                " with confidence " + response.outputs[0].data.concepts[0].value;
+                console.log("Response:" + reply);
+                // sendTextMessage(senderID, reply);
+                return '#' + reply;
+            },
+            function (err) {
+                console.log("Error:");
+                console.log(err);
+                // sendTextMessage(senderID, "I am unable to get any result from your input");
+                // return "I am unable to get any result from your input";
+            }
+        );
+    };
+
+    exports.create = function (url) {
+        appClarifai.inputs.create({
+            url: url,
+            concepts: [
+                {
+                    id: "Stybo",
+                    value: true
+                }
+            ]
+        });
+    };
 
 
-exports.generalModelSearch = function (searchUrl) {
+    exports.generalModelSearch = function (searchUrl) {
 
-    console.log("inside searchUrl" + searchUrl);
+        console.log("inside searchUrl" + searchUrl);
 
-    appClarifai.models.predict(Clarifai.GENERAL_MODEL, searchUrl).then(
-        function (response) {
-            console.log("General Model response =" + JSON.stringify(response));
-            // do something with response
-        },
-        function (err) {
-            console.log("General Model error =" + JSON.stringify(error));
+        appClarifai.models.predict(Clarifai.GENERAL_MODEL, searchUrl).then(
+            function (response) {
+                
+                console.log("General Model response =" + JSON.stringify(response));
+                // do something with response
+            },
+            function (err) {
+                console.log("General Model error =" + JSON.stringify(error));
 
-            // there was an error
-        }
-    );
-};
+                // there was an error
+            }
+        );
+    };
+
+}());
