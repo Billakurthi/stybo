a4aad27cccd9c87f911bc9c7e6f30819775810f6
billakurthi <avinash.billakurthi@gmail.com>
2017-10-12 16:40:42 +0530

    reply stirngify

--- services/apiaiService.js
+++ services/apiaiService.js
@@ -1,29 +1,32 @@
-
-var apiai = require('apiai');
-// const apiai_app = new ApiAI.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
-var app_apiai = apiai("30f26315bca54670ae2274a18e35bfa8");
+(function () {
 
 
-exports.callApiai = function (inputString, callback, senderID) {
+    var apiai = require('apiai');
+    // const apiai_app = new ApiAI.ApiAiClient({accessToken: '30f26315bca54670ae2274a18e35bfa8'});
+    var app_apiai = apiai("30f26315bca54670ae2274a18e35bfa8");
 
-    var request = app_apiai.textRequest(inputString, {
-        sessionId: '<unique session id>'
-    });
 
-    request.on('response', function (response) {
-        var reply = response.result.fulfillment.speech;
-        if (reply) {
-            console.log("Full api result: " + JSON.parse(reply,null,2));
-            callback(senderID, reply);
-        } else {
-            callback(senderID, "no entities trained");
-        }
-    });
+    exports.callApiai = function (inputString, callback, senderID) {
 
-    request.on('error', function (error) {
-        console.log("Build Error Message: " + error);
-        callback(senderID, "Error");
-    });
+        var request = app_apiai.textRequest(inputString, {
+            sessionId: '<unique session id>'
+        });
 
-    request.end();
-};
+        request.on('response', function (response) {
+            var reply = response.result.fulfillment.speech;
+            if (reply) {
+                console.log("Full api result: " + JSON.stringify(reply, null, 2));
+                callback(senderID, reply);
+            } else {
+                callback(senderID, "no entities trained");
+            }
+        });
+
+        request.on('error', function (error) {
+            console.log("Build Error Message: " + error);
+            callback(senderID, "Error");
+        });
+
+        request.end();
+    };
+})();
